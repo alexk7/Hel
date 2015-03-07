5675ef2eeb5ec29460858c28c6132903b8f03df3
Alexandre Cossette <alex.cossette@gmail.com>
2015-03-07 11:06:13 -0500

    Add empty test main.cpp and Xcode project.

---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+#include <cassert>
+#include <iostream>
+#include <utility>
+#include <tuple>
+#include <type_traits>
+#include <cxxabi.h>
+#include <array>
+#include <stdexcept>
+#include <typeinfo>
+using namespace std;
+
+int main()
+{
+}
